f3f2db48d1a7fa5eef273ab5cd3107c81a7091ef
Daffa954 <124135109+Daffa954@users.noreply.github.com>
2026-02-09 15:03:44 +0700

    ok

--- src/index.ts
+++ src/index.ts
@@ -15,11 +15,7 @@ async function init() {
 
     // app.use(express.json());
     app.use(
-      cors({
-        origin: "http://localhost:3000", // Izinkan origin frontend Anda secara spesifik
-        credentials: true, // Wajib true agar browser mengizinkan request
-        methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
-      }),
+      cors(),
     );
     app.use(bodyParser.json());
     app.get("/", (req, res) => {
